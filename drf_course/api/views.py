333b26618066b419793972847a2af0d2a27547f3
soethan98 <soethan762@gmail.com>
2024-12-31 08:32:35 +0800

    Set up apis from product list and detail

--- drf_course/api/views.py
+++ drf_course/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Product,Order,OrderItem
+from rest_framework.response import Response
+from .serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+
+
 
 # Create your views here.
+@api_view(['GET'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products,many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request,pk):
+    product = get_object_or_404(Product,pk = pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
